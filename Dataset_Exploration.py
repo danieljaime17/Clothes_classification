07e9c3021cf9a97fe27e236bb7fa0fdbcb37fc8d
Dani Jaime <danijaime17@gmail.com>
2024-04-07 23:14:44 +0200

    the Dataset exploration now shows at the end the image using matplotlib

--- Dataset_Exploration.py
+++ Dataset_Exploration.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.datasets import fashion_mnist
+import matplotlib.pyplot as plt
 
 # this code explores the dataset 
 
@@ -17,3 +18,12 @@ print("Shape of test labels:", y_test.shape)
 print("Example of a training image:")
 print(x_train[0])  # Show the pixel matrix of the first training image
 print("Corresponding label:", y_train[0])  # Show the label of the first training image
+
+#Show the image
+plt.figure(figsize=(5, 5))
+plt.subplot(1, 1, 1)
+plt.xticks([])
+plt.yticks([])
+plt.grid(False)
+plt.imshow(x_train[0], cmap=plt.cm.binary)
+plt.show()
